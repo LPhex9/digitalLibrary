2e2369485096ca310ab90dec130eda919f7b853c
razvan <razvantantari@gmail.com>
2025-09-24 05:32:53 +0300

    implementare crud pentru modul autori

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { AuthorsService } from './authors.service';
 
 @Controller('authors')
-export class AuthorsController {}
+export class AuthorsController {
+  constructor(private readonly authorsService: AuthorsService) {}
+
+  @Get()
+  getAllAuthors() {
+    return this.authorsService.getAllAuthors();
+  }
+
+  @Post()
+  createAuthor(@Body() authorData: any) {
+    return this.authorsService.createAuthor(authorData);
+  }
+
+  @Get(':id')
+  getAuthor(@Param('id') id: string) {
+    return this.authorsService.getAuthor(id);
+  }
+
+  @Put(':id')
+  updateAuthor(@Param('id') id: string, @Body() authorData: any) {
+    return this.authorsService.updateAuthor(id, authorData);
+  }
+
+  @Delete(':id')
+  deleteAuthor(@Param('id') id: string) {
+    return this.authorsService.deleteAuthor(id);
+  }
+}
--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthorsService {}
+export class AuthorsService {
+  private authors = [
+    { id: 1, name: 'J.K. Rowling', nationality: 'British', booksWritten: 15 },
+    { id: 2, name: 'Stephen King', nationality: 'American', booksWritten: 65 }
+  ];
+
+  getAllAuthors() {
+    return { authors: this.authors, total: this.authors.length };
+  }
+
+  createAuthor(authorData: any) {
+    const newAuthor = { id: this.authors.length + 1, ...authorData };
+    this.authors.push(newAuthor);
+    return { message: 'Author created successfully', author: newAuthor };
+  }
+
+  getAuthor(id: string) {
+    return this.authors.find(author => author.id === parseInt(id));
+  }
+
+  updateAuthor(id: string, authorData: any) {
+    const authorIndex = this.authors.findIndex(author => author.id === parseInt(id));
+    if (authorIndex > -1) {
+      this.authors[authorIndex] = { ...this.authors[authorIndex], ...authorData };
+      return { message: 'Author updated successfully', author: this.authors[authorIndex] };
+    }
+    return { message: 'Author not found' };
+  }
+
+  deleteAuthor(id: string) {
+    const authorIndex = this.authors.findIndex(author => author.id === parseInt(id));
+    if (authorIndex > -1) {
+      const deletedAuthor = this.authors.splice(authorIndex, 1);
+      return { message: 'Author deleted successfully', author: deletedAuthor };
+    }
+    return { message: 'Author not found' };
+  }
+}
