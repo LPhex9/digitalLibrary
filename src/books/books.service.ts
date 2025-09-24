e540abd4e79dd19e7a1fff179d8bbc4d01e0dc59
razvan <razvantantari@gmail.com>
2025-09-24 05:15:53 +0300

    implementare CRUD la carti modul

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  private books = [
+    { id: 1, title: 'Sample Book', author: 'Author Name', available: true }
+  ];
+
+  getAllBooks() {
+    return { books: this.books, total: this.books.length };
+  }
+
+  addBook(bookData: any) {
+    const newBook = { id: this.books.length + 1, ...bookData };
+    this.books.push(newBook);
+    return { message: 'Book added successfully', book: newBook };
+  }
+
+  getBook(id: string) {
+    return this.books.find(book => book.id === parseInt(id));
+  }
+}
